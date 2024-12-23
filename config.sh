31d0459e3b16a239c5e42d6a1a48f5889cb256c2
mataberat <akuwibonoo@gmail.com>
2024-12-23 19:54:34 +0700

    feat: use vim as default editor

--- config.sh
+++ config.sh
@@ -4,8 +4,10 @@ export LC_ALL=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 # Set default editor for kubectl
-export EDITOR="code --wait"
-export KUBE_EDITOR="code --wait"
+# export EDITOR="code --wait"
+# export KUBE_EDITOR="code --wait"
+export EDITOR="nvim"
+export KUBE_EDITOR="nvim"
 
 # Disable Ansible Python warnings
 export PYTHONWARNINGS="ignore"
